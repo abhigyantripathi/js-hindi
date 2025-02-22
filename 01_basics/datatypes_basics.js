f802ab4ba0eeeea9864f5d90538a8d9e28e0ec65
Abhigyan Tripathi <146621194+abhigyantripathi@users.noreply.github.com>
2025-02-22 07:03:41 +0000

    datatype summary

---
+++ 01_basics/datatypes_basics.js
@@ -0,0 +1,16 @@
+// pritmitive datatypes:7
+// Number, Boolean, String, null, undefined, symbol,BigInt
+// non primitive: array, object, functions
+
+const heroes= ["ironman","spiderman","superman"];
+let myObj={
+    name:"Abhigyan",
+    age :22,
+}
+const myFunc= function() {
+    
+    console.log("heloo hii")
+
+}
+
+
