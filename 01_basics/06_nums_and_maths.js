f2701164bc762ee8e60f237028cad3448f8ea7a2
Abhigyan Tripathi <146621194+abhigyantripathi@users.noreply.github.com>
2025-02-23 16:52:27 +0000

    maths and number

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,13 @@
+const score= 100
+console.log(typeof score)
+
+const balance= new Number(100.34556)
+console.log( balance)
+console.log(balance.toPrecision(3))
+// console.log(Math.random())
+
+console.log(Math.floor((Math.random()*10) +1))
+
+const max=20
+const min=10
+console.log(Math.floor((Math.random()*(max-min+1)) +min))
