04d0d1fb5407a4f10b5891e1e10d5f7673b10b3d
Abhigyan Tripathi <146621194+abhigyantripathi@users.noreply.github.com>
2025-02-23 13:34:58 +0000

    strings in js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,4 @@
+const name="abhigyan"// 1st way of declaring a string
+const mail="abhigyan@mail.com"
+console.log(`hello my name is ${name} and my mail is ${mail}`)
+const gameName= new String('cricket')//2nd way of declaring a string
